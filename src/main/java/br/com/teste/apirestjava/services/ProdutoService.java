9d262627e44cb97e31f24a784be776d51a48044b
CibeleMartins <cibelemartins24@hotmail.com>
2023-01-16 09:57:44 -0300

    🚧 feat: add métodos do serviço de produto

--- src/main/java/br/com/teste/apirestjava/services/ProdutoService.java
+++ src/main/java/br/com/teste/apirestjava/services/ProdutoService.java
@@ -1,6 +1,7 @@
 package br.com.teste.apirestjava.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -24,4 +25,39 @@ public class ProdutoService {
 
         return produtoRepository.obterTodos();
     }
+
+    /**
+     * Método que aciona o repositório para obter um produto pelo id.
+     * @return retorna o produto encontrado pelo id.
+     */
+    public Optional<Produto> obterPorId(Integer id) {
+
+        return produtoRepository.obterPorId(id);
+    }
+
+    /**
+     * Método que aciona o repositório para cadatrar um produto.
+     * @return retorna o produto cadastrado.
+     */
+    public Produto cadastrar(Produto produto) {
+        // Poderia ter alguma regra de negocio para adicionar um produto aqui.
+        // Ex: obrigatório passar o nome do produto, ou um valor acima de 100
+
+       return produtoRepository.cadastrar(produto);
+    }
+
+    public void deletar(Integer id) {
+        // Poderia ter alguma lógica de validação
+        // Ex: ver se o usuário tem permissão para deletar.
+        
+        produtoRepository.deletar(id);
+    }
+
+    public Produto atualizar(Integer id, Produto produto) {
+
+      produto.setId(id);
+
+      return produtoRepository.atualizar(produto);
+    }
+
 }
