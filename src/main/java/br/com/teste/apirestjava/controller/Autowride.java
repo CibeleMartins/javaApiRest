59a29ae019d0a0bf85555d05156d4537549bde5f
CibeleMartins <cibelemartins24@hotmail.com>
2023-01-16 11:37:50 -0300

    🚧 feat: implementou o controller

---
+++ src/main/java/br/com/teste/apirestjava/controller/Autowride.java
@@ -0,0 +1,5 @@
+package br.com.teste.apirestjava.controller;
+
+public @interface Autowride {
+
+}
---
+++ src/main/java/br/com/teste/apirestjava/controller/ProdutoController.java
@@ -0,0 +1,31 @@
+package br.com.teste.apirestjava.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.teste.apirestjava.model.Produto;
+import br.com.teste.apirestjava.services.ProdutoService;
+
+@RestController
+@RequestMapping("/api/produtos")
+public class ProdutoController {
+
+    @Autowired
+    private ProdutoService produtoService;
+
+    @GetMapping
+    public List<Produto> obterTodos() {
+
+        return produtoService.obterTodos();
+    }
+    @PostMapping
+    public Produto cadastrar(@RequestBody Produto produto) {
+        return produtoService.cadastrar(produto);
+    }
+}
