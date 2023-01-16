b5b0eeaf612c9d40cf040c164009e10331072c8d
CibeleMartins <cibelemartins24@hotmail.com>
2023-01-16 11:42:16 -0300

    🚧 feat: add método que pega produto pelo id no controller

--- src/main/java/br/com/teste/apirestjava/controller/ProdutoController.java
+++ src/main/java/br/com/teste/apirestjava/controller/ProdutoController.java
@@ -1,9 +1,11 @@
 package br.com.teste.apirestjava.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +26,13 @@ public class ProdutoController {
 
         return produtoService.obterTodos();
     }
+
+    @GetMapping("/{id}")
+    public Optional<Produto> obterPorId(@PathVariable Integer id) {
+        
+        return produtoService.obterPorId(id);
+    }
+    
     @PostMapping
     public Produto cadastrar(@RequestBody Produto produto) {
         return produtoService.cadastrar(produto);
