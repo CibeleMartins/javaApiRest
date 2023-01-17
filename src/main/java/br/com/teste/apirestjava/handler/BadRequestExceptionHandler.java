dbc1d53abf7045a265cf9f72df55c3911902e281
CibeleMartins <cibelemartins24@hotmail.com>
2023-01-17 14:14:44 -0300

    🥅 feat: criou o handler para a exception de bad request

---
+++ src/main/java/br/com/teste/apirestjava/handler/BadRequestExceptionHandler.java
@@ -0,0 +1,22 @@
+package br.com.teste.apirestjava.handler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import br.com.teste.apirestjava.model.error.ErrorMessage;
+import br.com.teste.apirestjava.model.exception.ResourceBadRequestException;
+
+@ControllerAdvice
+public class BadRequestExceptionHandler {
+    
+    @ExceptionHandler(ResourceBadRequestException.class)
+    public ResponseEntity<?> BadRequestResponse(ResourceBadRequestException ex) {
+        
+        ErrorMessage error = new ErrorMessage("Bad Request", HttpStatus.BAD_REQUEST.value(), ex.getMessage());
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+}
