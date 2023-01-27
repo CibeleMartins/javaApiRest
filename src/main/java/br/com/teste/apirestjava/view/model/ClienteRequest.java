9649448e8655d2cbe9a689b934c68c2f7e9e3403
CibeleMartins <cibelemartins24@hotmail.com>
2023-01-27 13:53:22 -0300

    🚧 feat: criou o modelo Resquest e Response de cliente

---
+++ src/main/java/br/com/teste/apirestjava/view/model/ClienteRequest.java
@@ -0,0 +1,75 @@
+package br.com.teste.apirestjava.view.model;
+
+public class ClienteRequest {
+
+    private String nome;
+
+    private String sobrenome;
+
+    private String email;
+
+    private String cpf;
+
+    private String endereco;
+
+    private String complemento;
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getNumeroTelefone() {
+        return numeroTelefone;
+    }
+
+    public void setNumeroTelefone(String numeroTelefone) {
+        this.numeroTelefone = numeroTelefone;
+    }
+
+    private String numeroTelefone;
+    
+}
---
+++ src/main/java/br/com/teste/apirestjava/view/model/ClienteResponse.java
@@ -0,0 +1,44 @@
+package br.com.teste.apirestjava.view.model;
+
+public class ClienteResponse {
+    private Integer id;
+
+    private String nome;
+
+    private String sobrenome;
+    
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+
+    public String getNumeroTelefone() {
+        return numeroTelefone;
+    }
+
+    public void setNumeroTelefone(String numeroTelefone) {
+        this.numeroTelefone = numeroTelefone;
+    }
+
+    private String numeroTelefone;
+}
